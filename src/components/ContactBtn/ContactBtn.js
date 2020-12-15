fe2b89fdbf2fdea5d6785128b4c22926bbf4c082
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2020-12-15 01:32:33 -0300

    add contact button

--- src/components/ContactBtn/ContactBtn.js
+++ src/components/ContactBtn/ContactBtn.js
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import styles from './ContactBtn.module.css'
+import { AnimateSharedLayout, motion } from 'framer-motion'
+
+const contactVariants = {
+    initial: {
+        borderRadius: 0,
+        color: ["#f4f4f4", "#061D3A"],
+        transition: {
+            duration: 0.5,
+            ease: 'easeInOut'
+        }
+    },
+    animate: {
+        borderRadius: 30,
+        color: ["#f4f4f4", "#061D3A"],
+        transition: {
+            duration: 0.5,
+            ease: 'easeInOut'
+        }
+    }
+}
 
 const ContactBtn = () => {
     const [scrolled, setScrolled] = useState(false)
 
+    const handleScroll = () => {
+        if(window.scrollY > 300) setScrolled(true)
+        else setScrolled(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [])
+
     return (
-        <>  
-            {scrolled ? (
-                <button
-                    onClick={e => { setScrolled(!scrolled) }}
-                    className={`${styles.button} ${styles.fixedButton}`}
-                >@</button>
-            ) : (
-                <button
-                    onClick={e => { setScrolled(!scrolled) }}
-                    className={`${styles.button} ${styles.staticButton}`}
-                >Entre em contato</button>
-            )}
-        </>
+        <AnimateSharedLayout type="crossfade">  
+            <motion.button
+                layout
+                className={`${styles.button} ${scrolled ? styles.fixedButton : styles.staticButton}`}
+                variants={contactVariants}
+                initial={false}
+                animate={scrolled ? "animate" : "initial"}
+            >{scrolled ? '@' : 'Entre em contato'}</motion.button>
+        </AnimateSharedLayout>
     );
 }
  
