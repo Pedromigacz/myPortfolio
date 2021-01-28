77602b10e239d2e3eecf6f215d53e5c70026fe7c
Pedro H G Migacz <pedromigacz@alunos.utfpr.edu.br>
2021-01-28 13:08:48 -0300

    make seo statically generated

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -1,88 +1,74 @@
-import React from 'react';
-import { Helmet } from 'react-helmet'
-import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import {Helmet} from 'react-helmet'
 
-const SEO = () => {
-    const {site: { siteMetadata: { description, title, url, image } }} = useStaticQuery(graphql`
-    {
-        site {
-        siteMetadata {
-            description
-            title
-            url
-            image
-        }
-        }
-    }
-    `)
+const SEO = ({description, title, url, image}) => {
 
-    return ( 
+
+    return(
         <Helmet
             title={title}
-            meta={
-                [
-                    {
-                        name: 'description',
-                        content: description,
-                    },
-                    // Facebook open graph
-                    {
-                        property: 'fb:app_id',
-                        content: '880823279352753',
-                    },
-                    {
-                        property: 'og:type',
-                        content: 'website',
-                    },
-                    {
-                        property: 'og:url',
-                        content: url,
-                    },
-                    {
-                        property: 'og:title',
-                        content: title,
-                    },
-                    {
-                        property: 'og:image',
-                        content: image,
-                    },
-                    {
-                        property: 'og:description',
-                        content: description,
-                    },
-                    // Twitter crawler stuff
-                    {
-                        name: 'twitter:card',
-                        content: "summary_large_image",
-                    },
-                    {
-                        name: 'twitter:title',
-                        content: title,
-                    },
-                    {
-                        name: 'twitter:description',
-                        content: description,
-                    },
-                    {
-                        name: 'twitter:image',
-                        content: image,
-                    },
-                    {
-                        name: 'twitter:site',
-                        content: '@pedgoshini',
-                    },
-                    {
-                        name: 'twitter:creator',
-                        content: '@pedgoshini',
-                    },
-                    {
-                        name: 'twitter:domain',
-                        content: 'pedromigacz.com',
-                    },
-                ]
-            }
-        />
+            meta={[
+                {
+                name: "description",
+                content: description
+                },
+                // Facebook open graph
+                {
+                property: "fb:app_id",
+                content: "880823279352753"
+                },
+                {
+                property: "og:type",
+                content: "website"
+                },
+                {
+                property: "og:url",
+                content: url
+                },
+                {
+                property: "og:title",
+                content: title
+                },
+                {
+                property: "og:image",
+                content: image
+                },
+                {
+                property: "og:description",
+                content: description
+                },
+                // Twitter crawler stuff
+                {
+                name: "twitter:card",
+                content: "summary_large_image"
+                },
+                {
+                name: "twitter:title",
+                content: title
+                },
+                {
+                name: "twitter:description",
+                content: description
+                },
+                {
+                name: "twitter:image",
+                content: image
+                },
+                {
+                name: "twitter:site",
+                content: "@pedgoshini"
+                },
+                {
+                name: "twitter:creator",
+                content: "@pedgoshini"
+                },
+                {
+                name: "twitter:domain",
+                content: "pedromigacz.com"
+                }
+            ]}
+            />
     )
 }
- 
+
 export default SEO
